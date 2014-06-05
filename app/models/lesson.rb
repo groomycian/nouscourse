5bcb55dc11cd5c22bc4a5fa9b58b980e0d5c35ba
Vladimir <sirvantos@gmail.com>
2014-06-05 21:38:50 +0400

    Added scope to order lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,8 @@
 class Lesson < ActiveRecord::Base
   belongs_to :course
 
+  default_scope -> { order('"order" ASC') }
+
   validates :name, presence: true, length: { maximum: 255 }
   validates :order, presence: true, :numericality => {:only_integer => true}
   validates :course_id, presence: true, :numericality => {:only_integer => true}

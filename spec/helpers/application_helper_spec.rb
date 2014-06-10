ac4befb6eec1020b4d336964954460fb8bec4611
Vladimir <sirvantos@gmail.com>
2014-06-10 16:00:16 +0400

    Shown all lessons by default

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+  def generate_lessons_data(course)
+    let(:another_course) { FactoryGirl.create(:course) }
+
+    before do
+      60.times { |i| FactoryGirl.create(:lesson, course: course, order: i) }
+      60.times { |i| FactoryGirl.create(:lesson, course: another_course, order: i) }
+
+      first(:link, course.name).click
+    end
+
+    after { Lesson.delete_all }
+  end
 end
